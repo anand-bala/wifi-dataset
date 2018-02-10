eb5fdfba0b18f6c7a86a5c7a24a78ad6b6f1a922
Anand Balakrishnan <anandbala1597@gmail.com>
2018-02-10 15:24:10 -0500

    Rm carto_ws; write script to setup cartographer

---
+++ install_cartographer.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Install wstool and rosdep.
+sudo apt-get update
+sudo apt-get install -y python-wstool python-rosdep ninja-build
+
+source /opt/ros/indigo/setup.bash
+
+mkdir -p carto_ws/src
+cd carto_ws
+wstool init src
+
+# Merge the cartographer_ros.rosinstall file and fetch code for dependencies.
+wstool merge -t src ../env/carto.rosinstall
+wstool update -t src
+
+# Install proto3.
+src/cartographer/scripts/install_proto3.sh
+
+# Install deb dependencies.
+# The command 'sudo rosdep init' will print an error if you have already
+# executed it since installing ROS. This error can be ignored.
+sudo rosdep init
+rosdep update
+rosdep install --from-paths src --ignore-src --rosdistro=${ROS_DISTRO} -y
+
+# Build and install.
+catkin_make_isolated --install --use-ninja
+
