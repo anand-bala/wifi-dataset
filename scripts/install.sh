ea997baadd673009fe8c6ed2e5e6aa18899e2c22
Anand Balakrishnan <anandbal@buffalo.edu>
2018-02-22 00:39:37 -0500

    Corrected install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,26 +1,31 @@
-#!/bin/bash
+#!/bin/bash -e
 
+working_dir=${PWD}
 
 if [ -d "./catkin_ws" ]; then
-   cd .
- elif [ -d "../catkin_ws" ]; then
-   cd ..
- else
-   echo "You're running the script from an invalid dir"
-   echo "Run it from the base of the wifi-dataset repo"
-   exit 1
- fi
+  working_dir=$(realpath .)
+elif [ -d "../catkin_ws" ]; then
+  working_dir=$(realpath ..)
+else
+  echo "You're running the script from an invalid dir"
+  echo "Run it from the base of the wifi-dataset repo"
+  exit 1
+fi
 
-source /opt/ros/indigo/setup.bash
+
+cd $working_dir
+
 git submodule update --init
 
+source /opt/ros/indigo/setup.bash
 
-sudo apt-get update && apt-get install -y \
-  python-wstool python-rosdep ninja-build \
-  readline
+sudo apt-get update && sudo apt-get install -y \
+  python-wstool python-rosdep ninja-build
 
 mkdir -p carto_ws/src && \
   wstool init carto_ws/src env/carto.rosinstall
+
+wstool update -t carto_ws/src
 
 ./carto_ws/src/cartographer/scripts/install_proto3.sh
 
@@ -33,10 +38,10 @@ cd carto_ws && \
   catkin_make_isolated --install --use-ninja
 
 
+
 rosdep install --from-paths catkin_ws/src --ignore-src --rosdistro=${ROS_DISTRO} -y;
 
 cd catkin_ws && \
-  source /opt/ros/indigo/setup.bash && \
   catkin_make install
 
 
