5c019cc59d87ea97c35915874115f35627e01641
Anand Balakrishnan <anandbala1597@gmail.com>
2018-04-04 16:29:44 -0400

    Minor stuff

--- env/setup.sh
+++ env/setup.sh
@@ -1,95 +1,52 @@
 #!/bin/bash
 
-function turtlebot_master {
-  export ROS_MASTER_URI="http://10.42.0.1:11311"
-  export ROS_IP=`hostname -I | head -n1 | cut -d " " -f1`
-}
-
-function open_vlp_feed {
-  sudo nmcli con up iface eth0 id vlp16
-}
-
-
-function init_workspace {
-  if [ "$#" -lt 2 ]; then
-    echo "Incorrect number of arguments"
-    echo "usage: init_workspace <path/to/workspace> <rosinstall file>"
-    return 1
-  fi
-  local ws="$1"
-  local rosinst="$2"
-  wstool init $ws/src 
-  wstool merge -t $ws/src $rosinst
-  return 0
-}
-
-function ws_source {
-  local ws1=""
-  local ws2=""
-  if [ -d "./catkin_ws" ] && [ -d "./carto_ws" ]; then
-    local ws1=./catkin_ws
-    local ws2=./carto_ws
-  elif [ -d "../catkin_ws" ] && [ -d "../carto_ws" ]; then
-    local ws1=../catkin_ws
-    local ws2=../carto_ws
-  else
-    echo "There seems to be some problem!"
-    echo "The workspace either has not been initialized correctly"
-    echo "Or you are not in the workspace directory"
-    return
-  fi
-  
-  echo "Sourcing the following ROS workspaces"
-  echo $ws1
-  echo $ws2
-  local src1=""
-  local src2=""
-  if [ -d "$ws1/install_isolated" ]; then
-    local src1="$ws1/install_isolated"
-  elif [ -d "$ws1/install" ]; then
-    local src1="$ws1/install"
-  elif [ -d "$ws1/devel_isolated" ]; then
-    local src1="$ws1/devel_isolated"
-  elif [ -d "$ws1/devel" ]; then
-    local src1="$ws1/devel"
-  else
-    echo "Please build the workspace in: $ws1"
-   return
-  fi
-  if [ -d "$ws2/install_isolated" ]; then
-    local src2="$ws2/install_isolated"
-  elif [ -d "$ws2/install" ]; then
-    local src2="$ws2/install"
-  elif [ -d "$ws2/devel_isolated" ]; then
-    local src1="$ws2/devel_isolated"
-  elif [ -d "$ws2/devel" ]; then
-    local src1="$ws2/devel"
-  else
-    echo "Please build the workspace in: $ws2"
-    return
-  fi
-  source $src2/setup.sh
-  source $src1/setup.sh --extend
-}
-
-
-function carto_asset_writer {
-  if [ "$#" -lt 2 ]; then
-    echo "Insufficient number of arguments";
-    echo "usage: carto_asset_writer <bagfile> <pose_graph>";
-    return
-  fi
-
-  echo "Reading bagfile: `realpath $1`"
-  echo "Writing to file: `realpath $2`"
+ws1=""
+ws2=""
+if [ -d "./catkin_ws" ] && [ -d "./carto_ws" ]; then
+  ws1=./catkin_ws
+  ws2=./carto_ws
+  export DATASET_WS=`realpath .`
+elif [ -d "../catkin_ws" ] && [ -d "../carto_ws" ]; then
+  ws1=../catkin_ws
+  ws2=../carto_ws
+  export DATASET_WS=`realpath ..`
+else
+  echo "There seems to be some problem!"
+  echo "The workspace either has not been initialized correctly"
+  echo "Or you are not in the workspace directory"
   return
+fi
 
-  $(ws_source)
-  echo "ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}"
-  roslaunch dataset_ros assets_writed_turtlebot_3d.launch \
-	bag_filename:=`realpath $1` \
-	pose_graph_filename:=`realpath $2`
-}
+echo "Sourcing the following ROS workspaces"
+echo $ws1
+echo $ws2
+src1=""
+src2=""
+if [ -d "$ws1/install_isolated" ]; then
+  src1="$ws1/install_isolated"
+elif [ -d "$ws1/install" ]; then
+  src1="$ws1/install"
+elif [ -d "$ws1/devel_isolated" ]; then
+  src1="$ws1/devel_isolated"
+elif [ -d "$ws1/devel" ]; then
+  src1="$ws1/devel"
+else
+  echo "Please build the workspace in: $ws1"
+  return
+fi
+if [ -d "$ws2/install_isolated" ]; then
+  src2="$ws2/install_isolated"
+elif [ -d "$ws2/install" ]; then
+  src2="$ws2/install"
+elif [ -d "$ws2/devel_isolated" ]; then
+  src1="$ws2/devel_isolated"
+elif [ -d "$ws2/devel" ]; then
+  src1="$ws2/devel"
+else
+  echo "Please build the workspace in: $ws2"
+  return
+fi
+source $src2/setup.sh
+source $src1/setup.sh --extend
 
-ws_source
 
