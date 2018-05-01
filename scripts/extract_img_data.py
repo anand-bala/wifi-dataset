52a4229f17cabb4c098f8dbd1279a5b982aa53d1
Anand Balakrishnan <anandbala1597@gmail.com>
2018-05-01 11:30:57 -0400

    Add extract img data to raw data

---
+++ scripts/extract_img_data.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+from datetime import datetime
+import os
+import sys
+
+import rosbag
+import rospy
+import cv2
+from cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import Image
+
+
+def mkdir_p(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+class Extractor:
+    def __init__(self, output_dir):
+        # type: (str)
+        self.output_dir = os.path.abspath(output_dir)
+
+        self.bridge = CvBridge()
+
+    def image_cb(self, topic, data, t):
+        # type: (str, Image, rospy.Time) -> None
+
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(data, desired_encoding="passthrough")
+        except CvBridgeError as e:
+            print(e)
+            sys.exit(1)
+
+        outfile = os.path.join(self.output_dir,
+                               datetime.fromtimestamp(t.to_sec()).strftime('%Y-%m-%d'),
+                               topic.strip("/"), str(t.secs), str(t) + '.png')
+        print("Saving to {}".format(outfile))
+        outdir = os.path.dirname(outfile)
+        print("Dir -> {}".format(outdir))
+        mkdir_p(outdir)
+        cv2.imwrite(outfile, cv_image)
+
+
+def main(output_dir, input_files):
+    # type: (str, list[str]) -> None
+    ex = Extractor(output_dir)
+    for bagfile in input_files:
+        bag = rosbag.Bag(bagfile)
+        for topic, msg, t in bag.read_messages():
+            if msg._type == 'sensor_msgs/Image':
+                ex.image_cb(topic, msg, t)
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Extract raw data from bag file')
+    parser.add_argument('bagfiles', nargs='+', help='input bag files')
+    parser.add_argument('-o', '--output', help='output file for bags.', default='raw_data')
+    args = parser.parse_args()
+
+    print(args.output)
+    print(args.bagfiles)
+    main(args.output, args.bagfiles)
